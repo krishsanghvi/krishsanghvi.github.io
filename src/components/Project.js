78f284818a070fca69530748257ad6b46f8d4e3c
Krish Sanghvi <krishksanghvi@gmail.com>
2024-01-02 20:57:38 -0500

    Project Descriptions

--- src/components/Project.js
+++ src/components/Project.js
@@ -13,16 +13,32 @@ import AccordionDetails from '@mui/material/AccordionDetails';
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 import Container from '@mui/material/Container';
 
+
+
 import { motion } from "framer-motion"
 
 
 const acord = {
-  "Code For Good":{description: "Description for Code for Good" },
-  "OSU Course Catalog":{description: "Description for OSU Course Catalog"},
-  "Personal Website":{description: "Built personal website to display skills, projects, and experiences in a professional React web application. Used GitHub Pages to host website for free."},
-  "S":{description: "Description for OSU Course Catalog"},
-  "OSU Course Catalog":{description: "Description for OSU Course Catalog"}
+  "Code For Good - Goodwill Columbus": {
+    description: "Developed a comprehensive system featuring user authentication, a dynamic course calendar, and a newsletter. Implemented secure password hashing and storage. Integrated a mentorship database, an intelligent chatbot, and an admin dashboard for real-time interactions and FAQs. Enabled Twilio push notifications, web page analytics, and event planning functionalities."
+  },
+  "OSU Course Catalog": {
+    description: "Enhanced the OSU Course Catalog system to provide an intuitive and user-friendly interface for course exploration and selection. Implemented advanced search capabilities and interactive course details."
+  },
+  "Personal Website": {
+    description: "Designed and built a professional personal website showcasing my skills, projects, and experiences. Developed using React and hosted on GitHub Pages, the site demonstrates my proficiency in modern web development."
+  },
+  "Pinball Game": {
+    description: "Programmed an engaging Pinball Game in C, integrating 2D motion physics, interactive graphics, and efficient input/output file handling for a seamless gaming experience."
+  },
+  "FEH Robot": {
+    description: "Engineered an autonomous robot using C++, designed to navigate and complete tasks on a challenging multifaceted course. The project showcased my skills in robotics and autonomous systems."
+  },
+  "To-Do List with Database": {
+    description: "Created a full-featured web application for task management. Features include adding, editing, and deleting tasks, with automatic categorization and data persistence. Developed with a focus on user experience and data integrity."
+  }
 }
+
 
 function Project() {
   return (
