10c5661ae7cb6bb2240d5fbddad9bf1ab78789f3
Krish Sanghvi <krishksanghvi@gmail.com>
2024-01-08 10:34:39 -0500

    Food Reviews!

--- src/components/Blog.js
+++ src/components/Blog.js
@@ -21,15 +21,25 @@ import { createTheme, ThemeProvider } from '@mui/material/styles';
 
 import MacAndCheese from "../media/Food Reviews/Mac&Cheese.jpg"
 
+import Minestrone_Soup from "../media/Food Reviews/Minestrone_Soup.jpg"
+
+
 const cards = [1, 2, 3, 4, 5, 6, 7, 8, 9];
-const foodReview = {
-  imageUrl: MacAndCheese,
-  title: 'Wisconsin Mac & Cheese from Noodles World Kitchen',
-  content: "A beautiful blend of cheddar and jack and cream sauce to create a fantastic choice for ala carte. Definitely grater than your midnight microwavable Mac & Cheese. And the cheddar is sharper than your boyfriend's jawline. You'll have to do some curls after eating this. It's not Gouda, it's grate! Overall, a solid 8/10! Sorry, I didn't want these puns to be too cheesy!"
+const foodReviews = {
+  "Wisconsin Mac & Cheese from Noodles World Kitchen": { 
+    image: MacAndCheese,
+    content: "A beautiful blend of cheddar and jack and cream sauce to create a fantastic choice for ala carte. Definitely grater than your midnight microwavable Mac & Cheese. And the cheddar is sharper than your boyfriend's jawline. You'll have to do some curls after eating this. It's not Gouda, it's grate! Overall, a solid 8/10! Sorry, I didn't want these puns to be too cheesy!"
+  },
+  "Mom's Magnificent Minestrone Soup": {
+    image: Minestrone_Soup, 
+    content: "A savory broth brewed with onions, carrots, zucchini, tomatoes, green beans, kidney beans, and shelled pasta, topped with a sprinkle of mozzarella cheese. It absouplutely destroys Olive Garden's version by a soup-slide, making it a soup-pendous choice! It's my soup-er star! This soup takes my broth away! 10/10! "
+  }
 };
 
-const sentences = foodReview.content.split('. ').filter(sentence => sentence);
-
+const splitIntoSentences = (content) => {
+  const sentenceRegex = /(?<=[.?!])\s/;
+  return content.split(sentenceRegex).filter(sentence => sentence);
+};
 // TODO remove, this demo shouldn't need to reset the theme.
 const defaultTheme = createTheme();
 
@@ -58,31 +68,35 @@ export default function Album() {
             </Typography>
           </Container>
         </Box>
-        <Box p={4}>
-      <Paper elevation={3}>
-        <Grid container spacing={2}>
-          <Grid item xs={12} md={6}>
-            <img 
-              src={foodReview.imageUrl} 
-              alt={foodReview.title} 
-              style={{ width: '100%', height: 'auto' }}
-            />
-          </Grid>
-          <Grid item xs={12} md={6}>
-            <Box p={2}>
-              <Typography align="center" variant="h5" gutterBottom>
-                {foodReview.title}
-              </Typography>
-              {sentences.map((sentence, index) => (
-                <Typography align="center" key={index} variant="body1" style={{ marginBottom: '0.5rem' }}>
-                  {sentence.trim()}.{index < sentences.length - 1 && <br />}
-                </Typography>
-              ))}
-            </Box>
-          </Grid>
-        </Grid>
-      </Paper>
-    </Box>
+        <Box p={8}>
+          {Object.entries(foodReviews).map(([name, { image, content }]) => (
+            <Paper square={false} elevation={4}>
+              <Grid container spacing={4}>
+
+                <Grid item xs={12} md={6}>
+                  <img 
+                    src={image} 
+                    alt={name} 
+                    style={{ width: '100%', height: 'auto' }}
+                  />
+                </Grid>
+                <Grid item xs={12} md={6}>
+                  <Box p={1}>
+                    <Typography align="center" variant="h5" gutterBottom sx={{ marginBottom: 4 }}>
+                      {name}
+                    </Typography>
+                    {splitIntoSentences(content).map((sentence, index) => (
+                    <Typography align="center" key={index} variant="body1" sx={{ marginBottom: 4 }}>
+                      {sentence.trim()}
+                    </Typography>
+                  ))}
+                   
+                  </Box>
+                </Grid>
+              </Grid>
+            </Paper>
+          ))}
+        </Box>
       </main>
       
       {/* End footer */}
